eacf3ba3b207e3e7bdb94928cff2018adb66011b
Lucas Aquino <lucas.silva22@live.com>
2022-02-21 11:39:31 -0300

    operadores combinados, de incremento e decremento

---
+++ php8-parte1/7-operadorespt2/a.php
@@ -0,0 +1,17 @@
+<?php
+# OPERADORES DE ATRIBUIÇÃO COMBINADOS
+# permitem alterar o valor de uma variável, a partir de seu valor original
+
+$a = 10;
+
+$a = $a + 20;
+$a = $a -3;
+$a = $a * 2;
+$a = $a / 2;
+
+// Simplificando...
+
+$a += 20;
+$a -=3;
+$a *= 2;
+$a /= 2;
---
+++ php8-parte1/7-operadorespt2/c.php
@@ -0,0 +1,16 @@
+<?php
+
+
+# OPERADORES DE INCREMENTO E DECREMENTO
+# Adicionam ou subtraem uma unidade à variável;
+
+# Os operadores podem ser de pré-incremento ou pós-incremento
+# o mesmo acontece para o decremento
+
+$a = 10;
+
+$a++;
+++$a;
+
+$a--;
+--$a;

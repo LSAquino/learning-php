5294b43270b4a23a5d321b1000f61812c91175c2
Lucas Aquino <lucas.silva22@live.com>
2022-02-21 12:39:50 -0300

    EXERCÍCIOS COM OPERADORES ARITMÉTICOS E DE ATRIBUIÇÃO

---
+++ php8-parte1/8-exerciciosOperadores/4.php
@@ -0,0 +1,15 @@
+<?php
+# 'a' é igual a 100
+# 'b' é igual a 250
+# 'c' é igual a 'a' mais 'b'
+# acrescentar mais uma unidade a 'c'
+# apresentar 'c'
+
+#resultado será 351
+
+$a = 100;
+$b = 250;
+$c = $a + $b;
+$c++;
+
+echo $c;

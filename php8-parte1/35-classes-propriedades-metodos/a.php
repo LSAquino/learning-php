25f541e0d54c5221a02fb9a188e31f5b0543c98f
Lucas Aquino <lucas.silva22@live.com>
2022-02-23 11:31:19 -0300

    classes, atributos e metodos

---
+++ php8-parte1/35-classes-propriedades-metodos/a.php
@@ -0,0 +1,44 @@
+<?php
+
+// CLASSES - INTRODUÇÃO À POO 
+
+
+#Teoricamente:
+// class Humano
+// cabelo
+// genero
+// peso
+// caminhar
+
+// Homem -> Humano
+// cabelo - castenho
+// genero - masculino
+// peso - 75
+// caminhar()
+
+// Mulher -> Humano
+// cabelo - claro
+// genero - feminino
+// peso - 63
+// caminhar()
+// cantar - pode ter funções adicionais
+
+
+class Humano
+{
+    // propriedades e métodos
+}
+class JogadorFutebol
+{
+    // propriedades e metodos
+}
+
+class FiguraGeometrica{
+    public $largura, $altura;
+    public $x;
+    public $y;
+
+    function novaArea($a, $b) {
+        return $a * $b;
+    }
+}

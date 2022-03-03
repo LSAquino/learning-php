8e9f112ee674f812327a83b4b9fec1b166bec37c
Lucas Aquino <lucas.silva22@live.com>
2022-03-03 13:51:03 -0300

    unix timestamp

---
+++ php8-intermediate-and-advanced/034-unix-timestamp/a.php
@@ -0,0 +1,13 @@
+<?php
+
+// UNIX TIMESTAMP
+/*
+Existe um valor numérico muito importante para as linguagens de programação e, de um modo especial para as bases de dados, que é o Unix Timestamp.
+Basicamente, o unix timestamp é o total de segundos decorridos desde 1 de janeiro de 1970, que é a data que marca o início da era do sistema Unix.
+*/
+
+echo time();
+
+// se quisermos o mesmo valor mas em microssegundos, podemos usar a seguinte função();
+
+// echo microtime(); 

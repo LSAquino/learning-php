a78e68daa17046b22358ac6ef7971ed0fdde8324
Lucas Aquino <lucas.silva22@live.com>
2022-03-03 13:39:27 -0300

    apresentação da data atual formatada

---
+++ php8-intermediate-and-advanced/034-data-atual/a.php
@@ -0,0 +1,11 @@
+<?php
+
+// DATAS E HORAS
+/*
+A utilização de datas e horas numa aplicação é fundamental e praticamente sempre presente em todos os projetos. No PHP existe a possibilidade de usarmos algumas funções mais antigas para a apresentação de datas e horas, ou utilizar uma metodologia mais próxima da programação orientada a objetos com a classe DateTime
+
+Vamos ver um exemplo simples de apresentação de uma data formatada.
+Neste caso, iremoas sempre usar a data do sistema.
+*/
+
+echo 'Empresa (c) ' . date('Y-m-d H:i:s');

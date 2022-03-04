aab771ebde61abe86836255d9a303447931869bb
Lucas Aquino <lucas.silva22@live.com>
2022-03-04 13:10:48 -0300

    push, pop, shift e unshift

---
+++ php8-intermediate-and-advanced/048-push-pop-shift-unshift/index.php
@@ -0,0 +1,30 @@
+<?php
+
+// ARRAYS - array_push(), array_pop(), array_shift(), array_unshift()
+
+/*
+Existem várias funções para manipular a inserção e remoção de valores de um array.
+Vejamos alguns exemplos
+*/
+
+$nomes = ['joao', 'ana', 'carlos'];
+
+// array_push() - permite acrescentar elementos no final do array
+// array_push($nomes, 'joaquim');
+// array_push($nomes, 'rui', 'marco', 'sandra', 25);
+// print_r($nomes);
+
+// array_unshift - permite adicionar elementos no início do array
+// array_unshift($nomes, 'tiago');
+// array_unshift($nomes, 'teresa', 'manuel');
+// print_r($nomes);
+
+// array_shift - retira um elemento do início do array
+// array_shift($nomes);
+// print_r($nomes);
+
+// array_pop - retira um elemento do final do array
+// array_pop($nomes);
+// print_r($nomes);
+
+// nestas 2 funções, os valores pode mser guardadas em variáveis

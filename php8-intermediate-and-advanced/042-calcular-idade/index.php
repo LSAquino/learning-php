b3e0dc298dade0729b5230491fe4b5ca10371c8f
Lucas Aquino <lucas.silva22@live.com>
2022-03-04 10:09:37 -0300

    como calcular idade

---
+++ php8-intermediate-and-advanced/042-calcular-idade/index.php
@@ -0,0 +1,37 @@
+<?php
+
+// EXEMPLO DE FUNÇÃO PARA CALCULAR A IDADE
+
+$data_de_nascimento = new DateTime('19-10-1973');
+
+echo calculate($data_nascimento);
+
+
+
+
+function calculate($data_nascimento)
+{
+
+    $agora = new DateTime();
+
+    // primeiro cálculo dos anos
+    if ($agora->format('Y') == $data_nascimento->format('Y')) {
+        // verifica se estamos no mesmo ano
+        $anos = 1;
+    } else {
+        $anos = $agora->format('Y') - $data_nascimento->format('Y');
+    }
+
+    // validar os anos de forma precisa
+    // se ainda não chegamos ao "nosso" mês, temos menos  ano do que é calculado
+
+    if ($agora->format('m') < $data_nascimento->format('d')) {
+        $anos--;
+    } elseif ($agora->format('m') == $data_nascimento->format('m')) {
+        // estamos no mês de aniversário, vamos ver se o dia já chegou
+        if ($agora->format('d') == $data_nascimento->format('d')) {
+            $anos--;
+        }
+    }
+    return $anos != 1 ? "$anos anos" : "1 ano";
+}

7d3c1476c788091f887339ff15e68ebe4188e28b
Lucas Aquino <lucas.silva22@live.com>
2022-03-07 11:41:02 -0300

    formas de controlar acesso a um script para tratar formulários

---
+++ php8-intermediate-and-advanced/066-controlar-acesso-a-um-script/tratar.php
@@ -0,0 +1,15 @@
+<?php
+
+// verificar que tipo de request foi usado
+if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+    die('Acesso negado.');
+}
+
+// verificar se existe os valores no POST
+if (!isset($_POST['usuario'])) {
+    header('Location: index.html');
+}
+$usuario = $_POST['usuario'];
+$senha = $_POST['usuario'];
+
+echo 'OK!';

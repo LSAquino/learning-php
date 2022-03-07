b0cd5254f0f6c9d609415550e34ac27cf54666ee
Lucas Aquino <lucas.silva22@live.com>
2022-03-07 11:10:19 -0300

    alguns métodos para contornar ausência de variáveis na query string

---
+++ php8-intermediate-and-advanced/064-query-string-ausensia-de-variaveis/index.php
@@ -0,0 +1,10 @@
+<?php
+
+// PASSAR VALORES ENTRE PÁGINAS WEB - VIA GET
+
+/*
+
+Todo o cuidado é pouco quando passamos variáveis por query string.
+Vejamos uma situação que pode comprometer a nossa página web e como solucionar o problema
+
+*/
---
+++ php8-intermediate-and-advanced/064-query-string-ausensia-de-variaveis/pagina2.php
@@ -0,0 +1,34 @@
+<?php
+
+if (!isset($_GET['nome'])) {
+    header('location: pagina1.php');
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página 2</title>
+</head>
+
+<body>
+
+    <h3>Página 2</h3>
+
+    <?php
+
+    $nome = htmlspecialchars($_GET['nome']);
+
+    ?>
+
+    <p><?= "$nome" ?></p>
+
+    <a href="pagina1.php">Voltar para a página 1 </a>
+
+</body>
+
+</html>

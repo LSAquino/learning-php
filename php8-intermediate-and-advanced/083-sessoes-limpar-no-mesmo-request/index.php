4f24e6e799a9d07c6377a2d415df78a27e3d68dc
Lucas Aquino <lucas.silva22@live.com>
2022-03-08 11:51:54 -0300

    limpar uma sessão no mesmo request

---
+++ php8-intermediate-and-advanced/083-sessoes-limpar-no-mesmo-request/index.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+
+echo '<pre>';
+
+// adicionar variáveis a uma sessão 
+$_SESSION['nome'] = 'joao';
+$_SESSION['valor'] = 1000;
+$_SESSION['frutas'] = ['banana', 'laranja', 'abacaxi'];
+print_r($_SESSION);
+echo '<hr>';
+
+// remover variáveis de uma sessão
+unset($_SESSION['valor']);
+print_r($_SESSION);
+echo '<hr>';
+
+// alterar o valor de uma variável de sessão
+$_SESSION['nome'] = 'Ribeiro';
+print_r($_SESSION);
+echo '<hr>';
+
+// apresentar variáveis de sessão
+echo 'Nome: ' . $_SESSION['nome'] . '<br>';
+echo 'Uma fruta: ' . $_SESSION['frutas'][1] . '<br>';
+echo '<hr>';
+
+// destruir todos os dados de uma sessão
+// session_destroy();
+session_unset();
+print_r($_SESSION);

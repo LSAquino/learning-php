1e861ba83ec603fd1166978c0307e103ffba94e2
Lucas Aquino <lucas.silva22@live.com>
2022-03-09 09:01:59 -0300

    conectar php ao mysql usando PDO

---
+++ php8-intermediate-and-advanced/084-pdo1/index.php
@@ -0,0 +1,9 @@
+<?php
+
+// definição da ligação
+$gestor = new PDO("mysql:host=localhost;dbname=loja","root","");
+$dados = $gestor->query("SELECT * FROM clientes");
+$clientes = $dados->fetchAll(PDO::FETCH_ASSOC);
+
+echo '<pre>';
+print_r($clientes);

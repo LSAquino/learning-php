0c9b05c7233c4262a45a083be63d450adc8798f2
Lucas Aquino <lucas.silva22@live.com>
2022-03-09 09:59:33 -0300

    apresentar dados de uma base de dados com php e html

---
+++ php8-intermediate-and-advanced/085-pdo-pt2/index.php
@@ -0,0 +1,46 @@
+<?php
+
+// definição da ligação
+$gestor = new PDO("mysql:host=localhost;dbname=loja;charset=utf8", "root", "");
+$dados = $gestor->query("SELECT * FROM clientes");
+$clientes = $dados->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h3>CLIENTE</h3>
+    <hr>
+    <table border="1">
+        <thead>
+            <tr>
+                <th>ID cliente</th>
+                <th>nome</th>
+            </tr>
+        </thead>
+        <tbody>
+
+            <?php foreach ($clientes as $cliente) : ?>
+
+                <tr>
+                    <td><?= $cliente['id_cliente'] ?></td>
+                    <td><?= $cliente['nome'] ?></td>
+                </tr>
+            <?php endforeach; ?>
+
+        </tbody>
+    </table>
+
+    <p>Total clientes: <strong><?= count($clientes) ?></strong></p>
+</body>
+
+</html>

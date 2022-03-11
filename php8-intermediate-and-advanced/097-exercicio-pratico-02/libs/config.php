43280c8492bf87e504f456c97afdf2f207cf8aa6
Lucas Aquino <lucas.silva22@live.com>
2022-03-11 10:11:34 -0300

    exercício prático parte 1, criação das condições genéricas, instalação do bootstrap, definição de constantes, entre outros..

---
+++ php8-intermediate-and-advanced/097-exercicio-pratico-02/libs/config.php
@@ -0,0 +1,5 @@
+<?php
+
+define('MYSQL_DATA','clube');
+define('MYSQL_USER','user_clube');
+define('MYSQL_PASS','f8845ew3YA5i');
---
+++ php8-intermediate-and-advanced/097-exercicio-pratico-02/public/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/bootstrap.min.css">
+    <link rel="shortcut icon" href="assets/favicon.png" type="image/png">
+    <title>CLUBE</title>
+</head>
+<body>
+    
+    <h3>CLUBE</h3>
+
+    <script src="assets/bootstrap.bundle.min.js"></script>
+</body>
+</html>
